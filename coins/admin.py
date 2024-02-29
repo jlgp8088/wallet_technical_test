9aab8f5ff7ffdae4cbab08d611ae434e0da8bcf3
Jose Guerrero <jlgp8088@gmail.com>
2024-02-29 01:01:52 +0100

    config admin panel  django

--- coins/admin.py
+++ coins/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from coins.models import Country,CryptoCoin,FiatCoin
 # Register your models here.
+
+admin.site.register(Country)
+admin.site.register(CryptoCoin)
+admin.site.register(FiatCoin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from users.models import UserFeeConfiguration
 # Register your models here.
+admin.site.register(UserFeeConfiguration)
